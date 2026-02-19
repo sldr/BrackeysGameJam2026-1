d9b4b34fa1482b86347fa2d80724dafe5bcf8aea
garrettderudder <garrettd1303@gmail.com>
2026-02-19 14:24:38 -0600

    Worked on level, black hole, and adjusted particles

---
+++ BlackHole.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class BlackHole : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void _on_area_2d_body_entered(PhysicsBody2D body)
+	{
+		if (body.IsInGroup("Player")) {
+			this.QueueFree();
+			
+		}
+	}
+}
